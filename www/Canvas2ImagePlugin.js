a8244ec480a59c2af8ef139602a5910f895a3056
agoodcoolman <agoodcoolman@163.com>
2017-08-24 16:52:58 +0800

    修改

--- www/Canvas2ImagePlugin.js
+++ www/Canvas2ImagePlugin.js
@@ -21,7 +21,7 @@ var exec = require('cordova/exec'),
         }
         else {
             
-            var imageData = canvas.toDataURL('image/jpeg').replace(/data:image\/png;base64,/,'');
+            var imageData = canvas.toDataURL('image/jpeg').replace(/data:image\/jpeg;base64,/,'');
             return cordova.exec(successCallback, failureCallback, "Canvas2ImagePlugin","saveImageDataToLibrary",[imageData]);
         }
     }
